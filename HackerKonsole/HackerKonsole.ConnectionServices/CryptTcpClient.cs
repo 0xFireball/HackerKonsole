c88433254a43cec0cfc6926a907b5a2730a599ee
0xFireball <0xFireball@outlook.com>
2016-05-14 23:34:03 -0700

    add stub crypttcpclient

---
+++ HackerKonsole/HackerKonsole.ConnectionServices/CryptTcpClient.cs
@@ -0,0 +1,21 @@
+﻿/*
+ */
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HackerKonsole.ConnectionServices
+{
+	/// <summary>
+	/// A virtualization layer over TcpClient that provides end-to-end encryption.
+	/// </summary>
+	public class CryptTcpClient
+	{
+		TcpClient _tcpClient;
+		
+		public CryptTcpClient(TcpClient existingTcpClient)
+		{
+			_tcpClient = existingTcpClient;
+		}
+	}
+}
