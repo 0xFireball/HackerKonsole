4fe3e6ccd02ceabf949e2b35834197f40d5998aa
0xFireball <0xFireball@outlook.com>
2016-05-17 13:34:41 -0700

    add connection capability to gui

--- HackerKonsole/HackerKonsole.Controller.GUI/MainForm.Designer.cs
+++ HackerKonsole/HackerKonsole.Controller.GUI/MainForm.Designer.cs
@@ -33,14 +33,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 316);
+            this.label1.Location = new System.Drawing.Point(9, 257);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(355, 17);
+            this.label1.Size = new System.Drawing.Size(264, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "(c) 2016 The ph4nt0m_sw0rd team. All rights reserved.";
             // 
@@ -48,9 +50,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(147, 69);
+            this.label2.Location = new System.Drawing.Point(110, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(203, 25);
+            this.label2.Size = new System.Drawing.Size(163, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Connect to HK Server";
             // 
@@ -58,32 +61,49 @@
             // 
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(119, 150);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.Location = new System.Drawing.Point(89, 122);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(268, 28);
+            this.textBox1.Size = new System.Drawing.Size(202, 24);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(182, 130);
+            this.label3.Location = new System.Drawing.Point(145, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(130, 17);
+            this.label3.Size = new System.Drawing.Size(95, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Address (host:port)";
             // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(137, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 27);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(501, 342);
+            this.ClientSize = new System.Drawing.Size(376, 278);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "HackerKonsole Controller GUI";
             this.ResumeLayout(false);
@@ -97,5 +117,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- HackerKonsole/HackerKonsole.Controller.GUI/MainForm.cs
+++ HackerKonsole/HackerKonsole.Controller.GUI/MainForm.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HackerKonsole.ConnectionServices;
+using HackerKonsole.Controller.Common;
 
 namespace HackerKonsole.Controller.GUI
 {
@@ -7,6 +12,53 @@ namespace HackerKonsole.Controller.GUI
         public MainForm()
         {
             InitializeComponent();
+            Form.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private async void button1_Click(object sender, System.EventArgs e)
+        {
+            await Task.Run(()=>ConnectToServer());
+        }
+
+        private void ConnectToServer()
+        {
+            button1.Text = "Connecting...";
+            button1.Enabled = false;
+            ConnectionInfo connInfo = null;
+            string remoteIp = textBox1.Text;
+            int portSepIndex = remoteIp.IndexOf(':');
+            int port = 25000;
+            if (portSepIndex > 0)
+            {
+                string[] hBits = remoteIp.Split(':');
+                port = int.Parse(hBits[1]);
+                remoteIp = hBits[0];
+            }
+            connInfo = new ConnectionInfo
+            {
+                RemoteHost = remoteIp,
+                RemotePort = port
+            };
+            using (var tcpConnection = new TcpClient(connInfo.RemoteHost, connInfo.RemotePort))
+            {
+                var cryptConnection = new CryptTcpClient(tcpConnection);
+                try
+                {
+                    Console.WriteLine("Attempting to establish connection...");
+                    cryptConnection.ClientPerformKeyExchange();
+                    Console.WriteLine("Connection successfully established!");
+
+                    var connectedController = new GuiConnectedControllerBackend(cryptConnection);
+                    connectedController.InitializeSession();
+                }
+                catch (Exception ex)
+                {
+                    cryptConnection.Close();
+                    MessageBox.Show("An error occurred with the connection: " + ex);
+                    button1.Text = "Connect";
+                    button1.Enabled = true;
+                }
+            }
         }
     }
 }
