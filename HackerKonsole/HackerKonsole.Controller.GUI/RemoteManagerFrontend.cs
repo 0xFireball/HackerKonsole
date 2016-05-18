8afc9132572cd93cb36919951742d3569d222800
Matthew Wong <matthew@adveca.com>
2016-05-17 23:15:04 -0700

    fixed renaming issue

---
+++ HackerKonsole/HackerKonsole.Controller.GUI/RemoteManagerFrontend.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HackerKonsole.Controller.GUI
+{
+    public partial class RemoteManagerFrontend : Form
+    {
+        public RemoteManagerFrontend()
+        {
+            InitializeComponent();
+        }
+
+        private void RemoteManagerFrontend_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
